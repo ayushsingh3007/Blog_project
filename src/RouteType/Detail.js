39f255a0738bbe0c26ba30a8bae214d5e0f59bb9
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-09-21 03:51:55 -0700

    Update Detail.js

--- src/RouteType/Detail.js
+++ src/RouteType/Detail.js
@@ -1,22 +1,11 @@
-import React, { useContext } from 'react';
-import { useParams } from 'react-router-dom';
-import { Store } from '../ContextStore/Storing';
+import React from 'react';
+
 
 const Details = () => {
-  const  id  = useParams().id; 
-  const { dataContext } = useContext(Store);
-
-
-
-  console.log(dataContext); 
-  return(<>
- {dataContext.filter((data)=>data.id===parseInt(id)).map((item,index)=>{
-    return(<>
-        <h1>{item.name}</h1>
-        <img src={item.cat} alt='not found'/>
-        
-        </>)
- })};
+ 
+return(
+  <>
+<h1></h1>
 
  </>
 
