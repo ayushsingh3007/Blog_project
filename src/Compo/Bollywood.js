b0314edd4d169cc3ae70770efa26b50ddf63b58f
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-09-21 03:34:51 -0700

    Update Bollywood.js

--- src/Compo/Bollywood.js
+++ src/Compo/Bollywood.js
@@ -1,73 +1,38 @@
 import React, { useContext } from 'react'
 import { Store } from '../ContextStore/Storing';
-
+import "./Style/Style.css"
 function Bollywood() {
     const [dataContext]=useContext(Store);
     console.log(dataContext);
 
   return (
     <>
-     <div id='container-1-home'>
-      {dataContext.filter((item)=>item.id===1).map((item,index)=>{
-        return(
-          <div id='home-item-1'>
-          
-          <img src={item.bolly_img} width="105%" height="90%" alt='no there'/>
-          <h3>{item.name}</h3>
-          {/* <p>{item.story}</p> */}
 
-
-          </div>
-        )
-      })}
-
-      {dataContext.filter((item)=>item.id===10).map((item,index)=>{
-      return(
-        <div id='home-item-2'>
-        <img src={item.bolly_img} width="70%"height="90%" alt='not there'/>
-        <h3>{item.name}</h3>
-        {/* <p>{item.story}</p> */}
-      
-      </div>
-      
-      )
-      })}
-         
-          {dataContext.filter((item)=>item.id===11).map((item,index)=>{
-            return(
-              <div id='home-item-3'>
-
-              <img src={item.bolly_img} width="70%" height="90%" alt='not there'/>
-              <h3>{item.name}</h3>
-              {/* <p>{item.story}</p> */}
-              
-              </div>
-            )
-          })}
-</div>
+    
+        
 <h3 id='the-latest'>The Latest</h3>
 <div id='latest-home'>
  
   <div id='latest-item'>
-    {dataContext.filter((item)=>item.id===7).map((item,index)=>{
+    {dataContext.filter((item)=>item.id===1).map((item,index)=>{
       return(
         <>
        
-        <img src={item.bolly_img} alt='not there' width="100%"/>
+        <img src={item.bolly_img} alt='not there' id='thelatest-img'/>
         <h4>{item.name}</h4>
-        <p>{item.story}</p>
+        <p>{item.story.slice(0,159)}....</p>
         </>
       )
     })} 
 </div>
 <div id='latest-item'>
-{dataContext.filter((item)=>item.id===6).map((item,index)=>{
+{dataContext.filter((item)=>item.id===2).map((item,index)=>{
       return(
         <>
         
-        <img src={item.bolly_img} alt='not there' width="100%"/>
+        <img src={item.bolly_img} alt='not there' id='thelatest-img'/>
         <h4>{item.name}</h4>
-        <p>{item.story}</p>
+        <p>{item.story.slice(0,159)}....</p>
         </>
       )
     })}
@@ -75,13 +40,13 @@ function Bollywood() {
 
     <div id='latest-item'>
 
-    {dataContext.filter((item)=>item.id===8).map((item,index)=>{
+    {dataContext.filter((item)=>item.id===6).map((item,index)=>{
       return(
       
     <>
-        <img src={item.bolly_img} alt='not there' width="100%" height="200rem"/>
+        <img src={item.bolly_img} alt='not there' id='thelatest-img'/>
         <h4>{item.name}</h4>
-        <p>{item.story}</p>
+        <p>{item.story.slice(0,159)}..........</p>
       </>  
       )
     })} 
@@ -91,11 +56,12 @@ function Bollywood() {
 <h3 id='top-stories'>Top Stories</h3>
 <div id='bottom-homepage'>
 <div id='bottom-item'>
-{dataContext.filter((item)=>item.id===11).map((item,index)=>{
+{dataContext.filter((item)=>item.id===2).map((item,index)=>{
   return(
     <>
-    <img src={item.bolly_img} alt='not there' width="200rem" id='bottom-img' />
-    <p>{item.story}</p>
+    <img src={item.bolly_img} alt='not there'  id='bottom-img' />
+    <h3>{item.name}</h3>
+    <p>{item.story.slice(0,159)}..........</p>
     
     </>
     
@@ -104,11 +70,12 @@ function Bollywood() {
 })}
 </div>
 <div id="bottom-item">
-{dataContext.filter((item)=>item.id===5).map((item,index)=>{
+{dataContext.filter((item)=>item.id===4).map((item,index)=>{
   return(
     <>
-    <img src={item.bolly_img} alt='not there' width="200rem"  id='bottom-img'/>
-    <p>{item.story}</p>
+    <img src={item.bolly_img} alt='not there'  id='bottom-img'/>
+    <h3>{item.name}</h3>
+    <p>{item.story.slice(0,159)}..........</p>
     
     </>
     
@@ -118,52 +85,58 @@ function Bollywood() {
 </div>
 
 <div id='bottom-item'>
+  {dataContext.filter((item)=>item.id===5).map((item,index)=>{
+    return(
+    <>
+    <img src={item.bolly_img} alt='not there'  id='bottom-img'/>
+    <h3>{item.name}</h3>
+    <span>{item.story.slice(0,159)}..........</span>
+    
+    </>)
+  })}
+</div>
+<div id='bottom-item'>
   {dataContext.filter((item)=>item.id===6).map((item,index)=>{
     return(
     <>
-    <img src={item.bolly_img} alt='not there' width="200rem" id='bottom-img'/>
-    <span>{item.story}</span>
+    <img src={item.bolly_img} alt='not there'  id='bottom-img'/>
+    <h3>{item.name}</h3>
+    <span>{item.story.slice(0,159)}..........</span>
     
     </>)
   })}
 </div>
 <div id='bottom-item'>
-  {dataContext.filter((item)=>item.id===4).map((item,index)=>{
+  {dataContext.filter((item)=>item.id===7).map((item,index)=>{
     return(
     <>
-    <img src={item.bolly_img} alt='not there' width="200rem" id='bottom-img'/>
-    <span>{item.story}</span>
+    <img src={item.bolly_img} alt='not there'  id='bottom-img'/>
+    <h3>{item.name}</h3>
+    
+    <span>{item.story.slice(0,159)}..........</span>
     
     </>)
   })}
 </div>
 <div id='bottom-item'>
-  {dataContext.filter((item)=>item.id===13).map((item,index)=>{
+  {dataContext.filter((item)=>item.id===5).map((item,index)=>{
     return(
     <>
-    <img src={item.bolly_img} alt='not there' width="200rem" id='bottom-img'/>
-    <span>{item.story}</span>
-    
-    </>)
-  })}
-</div>
-<div id='bottom-item'>
-  {dataContext.filter((item)=>item.id===9).map((item,index)=>{
-    return(
-    <>
-    <img src={item.bolly_img} alt='not there' width="200rem" id='bottom-img'/>
-    <span>{item.story}</span>
+    <img src={item.bolly_img} alt='not there'  id='bottom-img'/>
+    <h3>{item.name}</h3>
+    <span>{item.story.slice(0,159)}..........</span>
     
     </>)
   })}
 </div>
 
 <div id='bottom-item'>
-  {dataContext.filter((item)=>item.id===2).map((item,index)=>{
+  {dataContext.filter((item)=>item.id===13).map((item,index)=>{
     return(
     <>
-    <img src={item.bolly_img} alt='not there' width="200rem" id='bottom-img'/>
-    <span>{item.story}</span>
+    <img src={item.bolly_img} alt='not there'  id='bottom-img'/>
+    <h3>{item.name}</h3>
+    <span>{item.story.slice(0,159)}..........</span>
     
     </>)
   })}
@@ -188,34 +161,37 @@ function Bollywood() {
 <h3 id='top-post'>Top Posts</h3>
 <div id='bottom-right'>
    <div id="rigth-bottom-item-1">
-    {dataContext.filter((item)=>item.id===5).map((item,index)=>{
+    {dataContext.filter((item)=>item.id===10).map((item,index)=>{
       return(
       <>
-      <img src={item.bolly_img} alt='not found' width="250rem" id='img-right-bottom'/>
-      <p>{item.name}</p>
+      <img src={item.bolly_img} alt='not found'  id='img-right-bottom'/>
+      <h3>{item.name}</h3>
+      <p>{item.story.slice(0,90)}.....</p>
       <h1 id='number' >1</h1>
 
       </>
 )    })}
    </div>
    <div id="rigth-bottom-item-1">
-    {dataContext.filter((item)=>item.id===5).map((item,index)=>{
+    {dataContext.filter((item)=>item.id===11).map((item,index)=>{
       return(
       <>
-      <img src={item.bolly_img} alt='not found' width="250rem" id='img-right-bottom'/>
-      <p>{item.name}</p>
-      <h1 id='number' >1</h1>
+      <img src={item.bolly_img} alt='not found' id='img-right-bottom'/>
+      <h3>{item.name}</h3>
+      <p>{item.story.slice(0,90)}.....</p>
+      <h1 id='number-2' >2</h1>
 
       </>
 )    })}
    </div>
    <div id="rigth-bottom-item-1">
-    {dataContext.filter((item)=>item.id===5).map((item,index)=>{
+    {dataContext.filter((item)=>item.id===12).map((item,index)=>{
       return(
       <>
-      <img src={item.bolly_img} alt='not found' width="250rem" id='img-right-bottom'/>
-      <p>{item.name}</p>
-      <h1 id='number' >1</h1>
+      <img src={item.bolly_img} alt='not found'  id='img-right-bottom'/>
+      <h3>{item.name}</h3>
+      <p>{item.story.slice(0,90)}.....</p>
+      <h1 id='number-3' >3</h1>
 
       </>
 )    })}
@@ -228,6 +204,30 @@ function Bollywood() {
 
 <div id="advertise"><h1>Advertisement</h1></div>
 
+
+return(      
+<div id='footer'>
+  <div id='footer-item'>
+    <h3>Contact Us</h3>
+    <h4>Address:viajy nagar prayagraj </h4>
+    <p>Phone:0532-2678</p>
+    <p>support@134</p>
+
+  </div>
+  <div id='footer-item'>
+    <h3>Our Mission</h3>
+    <p>We are non profit association provided a good quality of education and give the correct direction to your carrer.....</p>
+  </div>
+  <div id='footer-item'>
+  <h3>SUPPORT</h3>
+    <h3>FAQ</h3>
+    <h3>Contact📞☎
+    </h3>
+  </div>
+</div>)     
+
+
+    
     </>
   )
 }
