6d1a5650ae86e13e49032749b24f522027ac46c5
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-09-21 03:36:50 -0700

    Update Hollywood.js

--- src/Compo/Hollywood.js
+++ src/Compo/Hollywood.js
@@ -1,73 +1,37 @@
 import React, { useContext } from 'react'
 import { Store } from '../ContextStore/Storing';
-
+import "./Style/Style.css"
 function Hollywood() {
   const [dataContext]=useContext(Store);
   console.log(dataContext)
   return (
     <>
     
-    <div id='container-1-home'>
-      {dataContext.filter((item)=>item.id===1).map((item,index)=>{
-        return(
-          <div id='home-item-1'>
-          
-          <img src={item.bolly_img} width="105%" height="90%" alt='no there'/>
-          <h3>{item.name}</h3>
-          {/* <p>{item.story}</p> */}
-
-
-          </div>
-        )
-      })}
-
-      {dataContext.filter((item)=>item.id===25).map((item,index)=>{
-      return(
-        <div id='home-item-2'>
-        <img src={item.holly_img} width="60%"height="90%" alt='not there'/>
-        <h3>{item.name}</h3>
-        {/* <p>{item.story}</p> */}
-      
-      </div>
-      
-      )
-      })}
+    
          
-          {dataContext.filter((item)=>item.id===31).map((item,index)=>{
-            return(
-              <div id='home-item-3'>
-
-              <img src={item.tech_img} width="60%" height="90%" alt='not there'/>
-              <h3>{item.name}</h3>
-              {/* <p>{item.story}</p> */}
-              
-              </div>
-            )
-          })}
-</div>
 <h3 id='the-latest'>The Latest</h3>
 <div id='latest-home'>
  
   <div id='latest-item'>
-    {dataContext.filter((item)=>item.id===7).map((item,index)=>{
+    {dataContext.filter((item)=>item.id===16).map((item,index)=>{
       return(
         <>
        
-        <img src={item.bolly_img} alt='not there' width="100%"/>
+        <img src={item.holly_img} alt='not there' id='thelatest-img'/>
         <h4>{item.name}</h4>
-        <p>{item.story}</p>
+        <p>{item.story.slice(0,159)}....</p>
         </>
       )
     })} 
 </div>
 <div id='latest-item'>
-{dataContext.filter((item)=>item.id===6).map((item,index)=>{
+{dataContext.filter((item)=>item.id===17).map((item,index)=>{
       return(
         <>
         
-        <img src={item.bolly_img} alt='not there' width="100%"/>
+        <img src={item.holly_img} alt='not there' id='thelatest-img'/>
         <h4>{item.name}</h4>
-        <p>{item.story}</p>
+        <p>{item.story.slice(0,159)}....</p>
         </>
       )
     })}
@@ -75,13 +39,13 @@ function Hollywood() {
 
     <div id='latest-item'>
 
-    {dataContext.filter((item)=>item.id===8).map((item,index)=>{
+    {dataContext.filter((item)=>item.id===18).map((item,index)=>{
       return(
       
     <>
-        <img src={item.bolly_img} alt='not there' width="100%" height="200rem"/>
+        <img src={item.holly_img} alt='not there' id='thelatest-img'/>
         <h4>{item.name}</h4>
-        <p>{item.story}</p>
+        <p>{item.story.slice(0,159)}..........</p>
       </>  
       )
     })} 
@@ -90,8 +54,177 @@ function Hollywood() {
 
 <h3 id='top-stories'>Top Stories</h3>
 <div id='bottom-homepage'>
+<div id='bottom-item'>
+{dataContext.filter((item)=>item.id===19).map((item,index)=>{
+  return(
+    <>
+    <img src={item.holly_img} alt='not there'  id='bottom-img' />
+    <h3>{item.name}</h3>
+    <p>{item.story.slice(0,159)}..........</p>
+    
+    </>
+    
+  )
+  
+})}
+</div>
+<div id="bottom-item">
+{dataContext.filter((item)=>item.id===20).map((item,index)=>{
+  return(
+    <>
+    <img src={item.holly_img} alt='not there'  id='bottom-img'/>
+    <h3>{item.name}</h3>
+    <p>{item.story.slice(0,159)}..........</p>
+    
+    </>
+    
+  )
+  
+})}
+</div>
+
+<div id='bottom-item'>
+  {dataContext.filter((item)=>item.id===21).map((item,index)=>{
+    return(
+    <>
+    <img src={item.holly_img} alt='not there'  id='bottom-img'/>
+    <h3>{item.name}</h3>
+    <span>{item.story.slice(0,159)}..........</span>
+    
+    </>)
+  })}
+</div>
+<div id='bottom-item'>
+  {dataContext.filter((item)=>item.id===22).map((item,index)=>{
+    return(
+    <>
+    <img src={item.holly_img} alt='not there'  id='bottom-img'/>
+    <h3>{item.name}</h3>
+    <span>{item.story.slice(0,159)}..........</span>
+    
+    </>)
+  })}
+</div>
+<div id='bottom-item'>
+  {dataContext.filter((item)=>item.id===23).map((item,index)=>{
+    return(
+    <>
+    <img src={item.holly_img} alt='not there'  id='bottom-img'/>
+    <h3>{item.name}</h3>
+    
+    <span>{item.story.slice(0,159)}..........</span>
+    
+    </>)
+  })}
+</div>
+<div id='bottom-item'>
+  {dataContext.filter((item)=>item.id===24).map((item,index)=>{
+    return(
+    <>
+    <img src={item.holly_img} alt='not there'  id='bottom-img'/>
+    <h3>{item.name}</h3>
+    <span>{item.story.slice(0,159)}..........</span>
+    
+    </>)
+  })}
+</div>
+
+<div id='bottom-item'>
+  {dataContext.filter((item)=>item.id===25).map((item,index)=>{
+    return(
+    <>
+    <img src={item.holly_img} alt='not there'  id='bottom-img'/>
+    <h3>{item.name}</h3>
+    <span>{item.story.slice(0,159)}..........</span>
+    
+    </>)
+  })}
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 </div>
+
+
+<h3 id='top-post'>Top Posts</h3>
+<div id='bottom-right'>
+   <div id="rigth-bottom-item-1">
+    {dataContext.filter((item)=>item.id===26).map((item,index)=>{
+      return(
+      <>
+      <img src={item.holly_img} alt='not found'  id='img-right-bottom'/>
+      <h3>{item.name}</h3>
+      <p>{item.story.slice(0,90)}.....</p>
+      <h1 id='number' >1</h1>
+
+      </>
+)    })}
+   </div>
+   <div id="rigth-bottom-item-1">
+    {dataContext.filter((item)=>item.id===27).map((item,index)=>{
+      return(
+      <>
+      <img src={item.holly_img} alt='not found' id='img-right-bottom'/>
+      <h3>{item.name}</h3>
+      <p>{item.story.slice(0,90)}.....</p>
+      <h1 id='number-2' >2</h1>
+
+      </>
+)    })}
+   </div>
+   <div id="rigth-bottom-item-1">
+    {dataContext.filter((item)=>item.id===28).map((item,index)=>{
+      return(
+      <>
+      <img src={item.holly_img} alt='not found'  id='img-right-bottom'/>
+      <h3>{item.name}</h3>
+      <p>{item.story.slice(0,90)}.....</p>
+      <h1 id='number-3' >3</h1>
+
+      </>
+)    })}
+   </div>
+
+
+
+
+</div>
+
+<div id="advertise"><h1>Advertisement</h1></div>
+
+
+        
+return(      
+<div id='footer'>
+  <div id='footer-item'>
+    <h3>Contact Us</h3>
+    <h4>Address:viajy nagar prayagraj </h4>
+    <p>Phone:0532-2678</p>
+    <p>support@134</p>
+
+  </div>
+  <div id='footer-item'>
+    <h3>Our Mission</h3>
+    <p>We are non profit association provided a good quality of education and give the correct direction to your carrer.....</p>
+  </div>
+  <div id='footer-item'>
+  <h3>SUPPORT</h3>
+    <h3>FAQ</h3>
+    <h3>Contact📞☎
+    </h3>
+  </div>
+</div>)
 
     
     </>
