4df0c213f4e7f57bb79ad316988d7ae08d18cc91
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-09-22 03:34:16 -0700

    Update Detail.js

--- src/RouteType/Detail.js
+++ src/RouteType/Detail.js
@@ -1,19 +1,44 @@
+import "./Details.css"
 import React, { useContext } from 'react';
-
 import { Store } from '../ContextStore/Storing';
+import {  useParams } from 'react-router-dom';
 
-const Details = () => {
-   
-  const { dataContext } = useContext(Store);
+const Detail = () => {
+  const [dataContext] = useContext(Store);
+  const { id, category } = useParams();
 
+  const selectedItem = dataContext.find(item => item.id === parseInt(id, 10));
 
+  if (!selectedItem) {
+    return <div>Item not found</div>;
+  }
 
-  console.log(dataContext); 
-  return(<>
-   <h1>i am ayush singh</h1>
- </>
+  const categoryData = dataContext.filter(item => item.cat === category).slice(0, 3); // Get the first three items.
 
-  )
+  return (
+    <div>
+      
+
+      <div id="top-div">
+        <span id="account-info"><img src="https://w7.pngwing.com/pngs/215/58/png-transparent-computer-icons-google-account-scalable-graphics-computer-file-my-account-icon-rim-123rf-symbol-thumbnail.png" alt="not found" id="account-icon"/></span>
+        
+        <h2>{selectedItem.name}</h2>
+        <img src={selectedItem.img} alt='not there' id='top-img' />
+        <p>{selectedItem.story}</p>
+      </div>
+
+    
+      <div id="flex">
+        {categoryData.map((item, index) => (
+          <div key={index} id="flex-item">
+            <img src={item.img} alt='not there' id="cat-img" />
+            <h4>{item.name}</h4>
+            <p>{item.story.slice(0, 159)}....</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Details;
+export default Detail;
